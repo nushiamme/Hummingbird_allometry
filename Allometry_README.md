94d7bc37c382ebd7f5938a877d70a3769d14e90d
Anusha Shankar <nushiamme@gmail.com>
2019-05-14 19:24:26 -0800

    Cleaning up code and formatting and adding info into README for publication in J Anim Ecol

--- Allometry_README.md
+++ Allometry_README.md
@@ -1,2 +1,23 @@
-# Hummingbird_allometry
-Code accompanying hummingbird daily energy expenditure allometry paper
+**Paper authors: A Shankar, DR Powers, LM Davalos, CH Graham**
+
+Code by: A Shankar, github/nushiamme; contact: nushiamme\<at\>gmail\<dot\>com for questions about code/datasets
+
+#### Code organisation
+
+-   **Allometry_phylo.R** - Needs input files *"DLW_TableS1.csv"* and *"hum294.tre"* (contact A Shankar for access); contains code plotting and analysing allometric relationship in hummingbirds, accounting for phylogenetic relatedness.
+    -   *Table 1*: Results from PGLS models (Brownian motion and Ornstein-Uhlenbeck models) of log(Daily energy expenditure) vs log(mass)
+    -   *Table 2*: Results of the MCMCglmm models with the Brownian-motion tree and without a phylogenetic tree. All the models used individual values, except one model which used species means.
+    -   *Fig. 1*: A log-log plot of individual and species-level hummingbird daily energy expenditure (kJ) vs. mass (g), including values from this study as well as from the literature. Includes species means and individual points.
+
+#### Packages you will need:
+
+    + MCMCglmm
+    + nlme
+    + ape
+    + geiger
+    + caper
+    + coda
+    + phytools
+    + tibble
+    + RColorBrewer
+    + ggplot2
--- Allometry_phylo.R
+++ Allometry_phylo.R
@@ -14,39 +14,45 @@ library(nlme)
 library(ape)
 library(geiger) # for treedata() function
 library(caper)
-library(coda) # only for autocorr function
+#library(coda) # only for autocorr function
 library(phytools)
-library(tibble) # To add columns to datasets with control
+#library(tibble) # To add columns to datasets with control
 library(RColorBrewer)
 library(ggplot2)
 
 #### Setup ####
 setwd("C:\\Users\\nushi\\Dropbox\\DLW_paper\\Data")
-## Read in torpor data file
-fmr_data <- read.csv("DLW_data2.csv") #Compiled daata from this paper and literature. Each row is an individual
+fmr_data <- read.csv("DLW_TableS1.csv") #Compiled data from this paper and literature. Each row is an individual
 
 ## Read in McGuire et al. 2014 hummingbird phylogeny
 tree_dlw<-read.tree("hum294.tre")
 #tre_ou_edited <- read.tree("OU_hummer_tree_FMR_edit.txt")
 
+## General plotting functions
+## Generic theme
+my_theme <- theme_classic(base_size = 30) + 
+  theme(panel.border = element_rect(colour = "black", fill=NA)) 
+
+## To add linear regression equation to plot
+lm_eqn <- function(y, x){
+  m <- lm(y ~ x);
+  eq <- substitute(italic(y) == 
+                     a + b %.% italic(x)*","~~italic(r)^2~"="~r2, 
+                   list(a = format(coef(m)[1], digits = 2), 
+                        b = format(coef(m)[2], digits = 2), 
+                        r2 = format(summary(m)$r.squared, digits = 3)))
+  as.character(as.expression(eq));                 
+}
+
+## Aggregating dataset by Species and site, to get species means of mass and daily energy expenditure (DEE)
 dlw_mean <- data.frame()
 dlw_mean <- aggregate(fmr_data$kJ_day, by=list(fmr_data$Species, fmr_data$Big_site), FUN="mean", na.omit=T)
 dlw_mass <- aggregate(fmr_data$Mass_g, by=list(fmr_data$Species, fmr_data$Big_site), FUN="mean", na.omit=T)
 dlw_mean <- merge(dlw_mean, dlw_mass, by = c("Group.1", "Group.2"))
 names(dlw_mean) <- c("Species", "Region", "kJ_day", "Mass_g")
 
-#### Phylogenetic components - prune tree ####
-#Replace tip names in the tree with those in torpor database
-## If using an updated tree check tree names again. Using a hard replace here because there aren't
-#too many to be replaced.
-#(To show just tip names, use: tree_dlw$tip.label)
-## To get tip numbers for the species in the DLW dataset, to then prune the phylogeny to match the dataset
-#head.species<-c("latirostris.3", "fulgens","clemenciae", "rubinoides", "imperatrix", "mellivora", "jacula", "coelestis","tzacatl", "urochrysia", "alexandri",
-#                "Calypte.anna","colombica.fannyae", "colombica.colombica", "benjamini", "yaruqui", "gigas")
-#ii<-sapply(head.species,grep,tree_dlw$tip.label)
-#ii
-
-## Manually replacing because it's a manageable number- trimming tree to DLW dataset
+## Trimming tree to DLW dataset
+## Manually replacing because it's a manageable number
 tree_dlw$tip.label[1]<-"FLME"
 tree_dlw$tip.label[15]<-"PHYA"
 tree_dlw$tip.label[83]<-"URBE"
@@ -54,7 +60,7 @@ tree_dlw$tip.label[92]<-"HEIM"
 tree_dlw$tip.label[93]<-"HERU"
 tree_dlw$tip.label[95]<-"HEJA"
 tree_dlw$tip.label[128]<-"AGCO"
-#tree_dlw$tip.label[154]<-"PAGI"
+#tree_dlw$tip.label[154]<-"PAGI" ## Doing this one separately later
 tree_dlw$tip.label[156]<-"EUFU"
 tree_dlw$tip.label[163]<-"LACL"
 tree_dlw$tip.label[185]<-"ARAL"
@@ -92,9 +98,9 @@ plot(tre1_noPgigas, cex=1.5, edge.width = 3)
 
 
 
-## May 2018 - Trying out GLS models with OU vs. Brownian motion
+## GLS models with OU vs. Brownian motion
 ## https://www.r-phylo.org/wiki/HowTo/PGLS
-## PGLS can only take one value per species, so I'm aggregating by mean DEE first, and then running the pgls model.
+## PGLS can only take one value per species, so aggregate by mean DEE first, and then run the pgls model.
 fmr.agg <- aggregate(fmr_data$kJ_day,by = list(fmr_data$Species), FUN='mean', na.rm=T)
 names(fmr.agg) <- c('Species', 'kJ_day')
 mass.agg <- aggregate(fmr_data$Mass_g,by = list(fmr_data$Species), FUN='mean', na.rm=T)
@@ -121,8 +127,6 @@ summary(ou.gls)
 plot(ou.gls$residuals)
 plot(ou.gls)
 
-### Test separate effects of phylogeny on Mass and phylogeny on FMR
-## Maybe with the alpha thing on the OU tree again
 
 #### Models ####
 ## Now, to run Bayesian models with repeated measures per species (i.e. multiple individuals per species), 
@@ -137,8 +141,8 @@ inv.phylo_noPgigas <- inverseA(tre1_noPgigas, nodes="TIPS", scale=TRUE)
 ## Make OU tree
 tre_ou <- rescale(tre1, model = "OU", alpha=48.13674) ## Alpha from running OU gls model above
 plot(tre_ou, cex=1.5, edge.width = 3)
-## Inverse matrix of the OU tree - doesn't work, edge lengths are supposedly still zero.
-inv.phylo_ou <-inverseA(tre_ou_edited,nodes="TIPS",scale=T)
+## Inverse matrix of the OU tree - doesn't work, edge lengths are zero => star phylogeny.
+#inv.phylo_ou <-inverseA(tre_ou_edited,nodes="TIPS",scale=T)
 
 #set up a prior for a phylogenetic mixed model
 #Setting priors to be very uninformative
@@ -157,18 +161,21 @@ levels(fmr_data$Big_site.f)[match("CH",levels(fmr_data$Big_site.f))] <- 1
 levels(fmr_data$Big_site.f)[match("CR",levels(fmr_data$Big_site.f))] <- 2
 levels(fmr_data$Big_site.f)[match("EC",levels(fmr_data$Big_site.f))] <- 3
 
+## Make a variable to compare temperate and tropical measurements 
 fmr_data$Temptrop <- as.factor(fmr_data$Big_site)
 levels(fmr_data$Temptrop)[match("AZ",levels(fmr_data$Temptrop))] <- 0
 levels(fmr_data$Temptrop)[match("CH",levels(fmr_data$Temptrop))] <- 1
 levels(fmr_data$Temptrop)[match("CR",levels(fmr_data$Temptrop))] <- 1
 levels(fmr_data$Temptrop)[match("EC",levels(fmr_data$Temptrop))] <- 1
 
+## Non-log model, just out of interest. Do not use!
 DEE_full_raw <-MCMCglmm(kJ_day~Mass_g+Temptrop, 
              random=~Species, ginverse = list(Species=inv.phylo$Ainv), 
              prior=prior, data=fmr_data, verbose=FALSE, nitt = 5000000, thin = 1000)
 summary(DEE_full_raw)
 plot(DEE_full_raw) 
 
+## Log-log full model
 DEE_log <-MCMCglmm(log(kJ_day)~log(Mass_g)+Temptrop, 
                     random=~Species, ginverse = list(Species=inv.phylo$Ainv), 
                     prior=prior, data=fmr_data, verbose=FALSE, nitt = 5000000, thin = 1000)
@@ -189,22 +196,21 @@ DEE_log_mass <-MCMCglmm(log(kJ_day)~log(Mass_g),
 summary(DEE_log_mass)
 plot(DEE_log_mass)
 
-## Dee-mass, log-log, no tree, and using only species means
+## DEE-mass, log-log, no tree, and using only species means
 DEE_log_mass_means <-MCMCglmm(log(kJ_day)~log(Mass_g), 
                         random=~Species, ginverse = list(Species=inv.phylo$Ainv), 
                         prior=prior, data=dlw_mean, verbose=FALSE, nitt = 5000000, thin = 1000)
 summary(DEE_log_mass_means)
 plot(DEE_log_mass_means)
 
-
-## Running the OU tree doesn't work, it says edge lengths are zero.
-## So running the MCMCglmm model without the tree, because the OU tree is a star phylogeny anyway
+## Now run the MCMCglmm model without the tree, because the OU tree yields a star phylogeny
 DEE_log_mass_noTree <-MCMCglmm(log(kJ_day)~log(Mass_g), 
-                        random=~Species, 
-                        prior=prior, data=fmr_data, verbose=FALSE, nitt = 5000000, thin = 1000)
+                               random=~Species, 
+                               prior=prior, data=fmr_data, verbose=FALSE, nitt = 5000000, thin = 1000)
 summary(DEE_log_mass_noTree)
 plot(DEE_log_mass_noTree)
 confint(DEE_log_mass_noTree)
+
 
 ## Derive R2 from MCMCglmm results
 ## Source: https://www.int-res.com/articles/suppl/m561p001_supp2.pdf
@@ -229,70 +235,9 @@ plot(DEE_log_mass_noTree_noPgigas)
 
 
 ## Plot temp and tropical individuals
-my_theme <- theme_classic(base_size = 30) + 
-  theme(panel.border = element_rect(colour = "black", fill=NA)) 
 colourCount <- length(unique(dlw_mean$Species))
 getPalette <- colorRampPalette(brewer.pal(9, "Set1"))
 
-lm_eqn <- function(y, x){
-  m <- lm(y ~ x);
-  eq <- substitute(italic(y) == 
-                     a + b %.% italic(x)*","~~italic(r)^2~"="~r2, 
-                   list(a = format(coef(m)[1], digits = 2), 
-                        b = format(coef(m)[2], digits = 2), 
-                        r2 = format(summary(m)$r.squared, digits = 3)))
-  as.character(as.expression(eq));                 
-}
-
-## Plotting temperate vs tropical hummingbird DEE
-ggplot(fmr_data, aes(Mass_g, kJ_day, col=Temptrop)) + my_theme +
-  geom_smooth(method='lm',se = F, size=2) + 
-  geom_point(size=2, alpha=0.7) +
-  geom_text(x = 5, y = 100, col="light blue",
-            label = lm_eqn(fmr_data$kJ_day[fmr_data$Temptrop==0],
-                           fmr_data$Mass_g[fmr_data$Temptrop==0]), parse=T) +
-  geom_text(x = 6, y = 150, col="black", 
-            label = lm_eqn(fmr_data$kJ_day[fmr_data$Temptrop==1],
-                           fmr_data$Mass_g[fmr_data$Temptrop==1]), parse=T) +
-  scale_color_manual(values= c("light blue", "black"), 
-                      name="Temperate / Tropical", labels=c("Temperate", "Tropical")) +
-  ylab("Daily Energy Expenditure (kJ/day)") + xlab("Initial mass (g)")
-
-## Plotting log-log temperate vs tropical hummingbird DEE
-ggplot(fmr_data, aes(log(Mass_g), log(kJ_day), col=Temptrop)) + my_theme +
-  geom_smooth(method='lm',se = F, size=2) + 
-  geom_point(size=2, alpha=0.7) +
-  geom_text(aes(label=Species)) +
-  geom_text(x = 1.5, y = 4, col="light blue",
-            label = lm_eqn(log(fmr_data$kJ_day[fmr_data$Temptrop==0]),
-                           log(fmr_data$Mass_g[fmr_data$Temptrop==0])), parse=T) +
-  geom_text(x = 1.75, y = 5, col="black", 
-            label = lm_eqn(log(fmr_data$kJ_day[fmr_data$Temptrop==1]),
-                           log(fmr_data$Mass_g[fmr_data$Temptrop==1])), parse=T) +
-  scale_color_manual(values= c("light blue", "black"), 
-                     name="Temperate / Tropical", labels=c("Temperate", "Tropical")) +
-  ylab("log(Daily Energy Expenditure (kJ/day))") + xlab("log(Initial mass (g))")
-
-dlw_mean$Temptrop <- as.factor(dlw_mean$Region)
-levels(dlw_mean$Temptrop)[match("AZ",levels(dlw_mean$Temptrop))] <- 0
-levels(dlw_mean$Temptrop)[match("CH",levels(dlw_mean$Temptrop))] <- 1
-levels(dlw_mean$Temptrop)[match("CR",levels(dlw_mean$Temptrop))] <- 1
-levels(dlw_mean$Temptrop)[match("EC",levels(dlw_mean$Temptrop))] <- 1
-
-## Species means. Regressions based on spp means vs individual values. What do they tell you? Benefits of having individual-level data. 
-ggplot(dlw_mean, aes(log(Mass_g), log(kJ_day))) + 
-  geom_point(aes(col=Species), size=5) + theme_bw() + xlab("Log(Mass (g))") +
-  #scale_shape_manual("Region\n", values=c(16, 3, 1, 2), labels=c("Arizona", "Chile", "Costa Rica", "Ecuador")) +
-  #scale_colour_manual(values = getPalette(colourCount)) +
-  geom_text(x = 1.5, y = 4, col="light blue",
-            label = lm_eqn(log(dlw_mean$kJ_day[dlw_mean$Temptrop==0]),
-                           log(dlw_mean$Mass_g[dlw_mean$Temptrop==0])), parse=T, size=8) +
-  geom_text(x = 1.75, y = 5, col="black", 
-            label = lm_eqn(log(dlw_mean$kJ_day[dlw_mean$Temptrop==1]),
-                           log(dlw_mean$Mass_g[dlw_mean$Temptrop==1])), parse=T, size=8) +
-  ylab("Log(kJ per day)")  + geom_smooth(aes(group=Temptrop), method=lm) + 
-  theme(strip.background = element_blank(), 
-        panel.border = element_rect(colour = "black", fill=NA), legend.key.height=unit(2,"line"))
 
 ## Good graph of species means and individual points, with regression line through them. 
 ggplot(NULL, aes(log(Mass_g), log(kJ_day))) + 
@@ -312,9 +257,6 @@ ggplot(NULL, aes(log(Mass_g), log(kJ_day))) +
   ylab("Log(kJ per day)")  + 
   theme(legend.key.height=unit(2,"line"))
 
-#Relationship without PAGI:
-lm(log(fmr_data$kJ_day[fmr_data$Temptrop==1 & fmr_data$Species != "PAGI"]) ~ 
-     log(fmr_data$Mass_g[fmr_data$Temptrop==1 & fmr_data$Species != "PAGI"]))
 
 ## t-test testing temperate vs. tropical sites:
 ## Raw DEE significant, but includes PAGI (t(60) = 5.03, p-value = 4.65e-06)
@@ -327,7 +269,3 @@ plot(an.fmr,5) # Reisduals vs. leverage. All points are well enough away from th
 aov_residuals <- residuals(object = an.fmr)
 # Run Shapiro-Wilk test
 shapiro.test(x = aov_residuals)
-
-## Trying out LM with species means
-lm.dlw_mean <- lm(log(kJ_day)~log(Mass_g), data=dlw_mean)
-summary(lm.dlw_mean)
